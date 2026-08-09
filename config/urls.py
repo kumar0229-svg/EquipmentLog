c99d52b256fc3d9edbda8ae6e6f145d09d47fda4
kumar0229 <kumar0229@gmail.com>
2026-08-10 04:30:46 +0530

    Rebrand app as DigiLog with Cipla logo
    Replace the generic "Equipment Activity Log" text and default Django
    admin branding with the Cipla logo and "DigiLog" name across the main
    app header, page titles, favicon, and admin site.

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+admin.site.site_header = 'DigiLog Administration'
+admin.site.site_title = 'DigiLog'
+admin.site.index_title = 'DigiLog Administration'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
